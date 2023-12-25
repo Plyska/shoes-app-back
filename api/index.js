253328738dc73d2031527d76671bc099f0d6f549
Andriy Plyska <andriyplyska@MBP-Andriy.home>
2023-12-26 00:14:50 +0100

    add all api methoods

--- api/index.js
+++ api/index.js
@@ -5,4 +5,16 @@ const getAllShoes = async () => {
     return await axios.get(url).then(res => res.data);
 };
 
-module.exports = { getAllShoes };
+const editShoes = async (shoesId, shoes) => {
+    return await axios.put(`${url}/${shoesId}`, shoes);
+};
+
+const addShoes = async (shoes) => {
+    return await axios.post(url, shoes).then(res => res.data);
+}
+
+const deleteShoes = async (shoesId) => {
+    return await axios.delete(`${url}/${shoesId}`);
+}
+
+module.exports = { getAllShoes, editShoes, addShoes, deleteShoes };
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { port } = require("./constants");
-const { getAllShoes } = require("./api");
+const { getAllShoes, editShoes, addShoes, deleteShoes } = require("./api");
+const bodyParser = require("body-parser");
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const corsOptions = {
   origin: "*",
@@ -22,13 +24,36 @@ app.get("/all-shoes", cors(corsOptions), async (_, res) => {
   }
 });
 
-app.post("/all-shoes", cors(corsOptions), async (_, res) => {
-    try {
-        // const { name, brand }
-    } catch (e) {
-      console.log(e);
-    }
-  });
+app.post("/all-shoes", cors(corsOptions), async (req, res) => {
+  try {
+    const shoes = req.body;
+    await addShoes(shoes);
+    res.status(200).send({ status: 200, message: "Shoes was added" });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.put("/all-shoes/:id", cors(corsOptions), async (req, res) => {
+  try {
+    const shoes = req.body;
+    const shoesId = req.params.id;
+    await editShoes(shoesId, shoes);
+    res.send({ status: "success" });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.delete("/all-shoes/:id", cors(corsOptions), async (req, res) => {
+  try {
+    const shoesId = req.params.id;
+    await deleteShoes(shoesId);
+    res.send({ status: "success", message: "Shoes was deleted" });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
