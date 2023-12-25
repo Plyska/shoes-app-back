0d02e3ec865c73bd4a67e087f48438f259de21b2
Andriy Plyska <andriyplyska@MBP-Andriy.home>
2023-12-25 23:38:53 +0100

    change api key

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const port = process.env.PORT;
-const api_key = process.env.APP_KEY;
+const api_key = process.env.API_KEY;
 const url = `https://crudcrud.com/api/${api_key}/shoes` 
 
 module.exports = { port, api_key, url }
